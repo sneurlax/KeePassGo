deff2e03aa3c10a804e96782644bdef79ad8667a
sneurlax <sneurlax@gmail.com>
2018-06-30 16:51:21 -0700

    added github.com/tobischo/gokeepasslib

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/tobischo/gokeepasslib"
+  "os"
+)
 
 func main() {
-  fmt.Println("Hello World")
+  file, _ := os.Open("test.kdbx")
+
+  db := gokeepasslib.NewDatabase()
+  db.Credentials = gokeepasslib.NewPasswordCredentials("test")
+  _ = gokeepasslib.NewDecoder(file).Decode(db)
+
+  db.UnlockProtectedEntries()
+
+  entry := db.Content.Root.Groups[0].Entries[0]
+  fmt.Println(entry.GetTitle())
+  fmt.Println(entry.Get("UserName"))
+  fmt.Println(entry.GetPassword())
 } 
